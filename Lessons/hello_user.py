5f65b4c02c73c6725fc2f357dc60d25e43d90a33
ruslangadaborsh <qa.gadaborshev@gmail.com>
2024-05-18 16:29:42 +0300

    updated hello_user.py

--- Lessons/hello_user.py
+++ Lessons/hello_user.py
@@ -2,6 +2,10 @@
 import sys
 
 # get first argument from command line and save it into name variable
-name = sys.argv[1]
+# name = sys.argv[1]
+# print("Hello, ", name, "!")
 
-print("Hello, ", name)
+# get some arguments from command line and save them into name variable
+first_name = sys.argv[1]
+last_name = sys.argv[2]
+print("Hello, ", first_name, last_name)
